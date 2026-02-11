407631b1cf3420801824a16eb7f8700b941cf5f2
Atila Fraga da Silva <ziofs91@gmail.com>
2026-02-10 21:48:20 -0300

    Ajuste no script de instalação do node exporter para linux

--- PROMETHEUS/prometheus-node-exporter-linux.sh
+++ PROMETHEUS/prometheus-node-exporter-linux.sh
@@ -4,4 +4,30 @@ cd /opt/observability
 wget https://github.com/prometheus/node_exporter/releases/download/v1.10.2/node_exporter-1.10.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.10.2.linux-amd64.tar.gz
 cd  node_exporter-1.10.2.linux-amd64
-./node_exporter &   
+mkdir -p /var/lib/node
+cp node_exporter /var/lib/node
+tee /etc/systemd/system/exporter.service<<EOF
+[Unit]
+Description=Prometheus Node Exporter
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/var/lib/node/node_exporter
+
+SyslogIdentifier=prometheus_node_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable exporter
+systemctl start exporter
+systemctl status exporter
+ss -tulpn | grep ":9100"
